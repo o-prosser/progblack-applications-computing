fa091131c362da66e8ace3644c39c355956429c1
Owen Prosser <owenprosser@icloud.com>
2025-11-04 09:29:25 +0000

    Add feedback buttons

---
+++ index.js
@@ -0,0 +1,10 @@
+const yesButton = document.querySelector("#yesButton");
+const noButton = document.querySelector("#noButton");
+
+yesButton.addEventListener("click", () => {
+  alert("That's wonderful");
+});
+
+noButton.addEventListener("click", () => {
+  alert("You wanted to say no!!!!!! Watch out for the Lennox!!!!");
+});
